6b299c2b6f24f1d8d1a2da334b7ed1837f4597e0
Prudhvi Kommareddi <pkommareddi@agiliteksolutions.com>
2026-08-05 18:56:05 -0700

    Use ReviewApp3 service layers

--- src/runtime/types.tsx
+++ src/runtime/types.tsx
@@ -31,10 +31,10 @@ export enum DataSourceLabel {
   USAGE_TYPE = 'Usage Type',
   PRESENCE_MARKUP = 'Presence Markup',
   PRESENCE = 'Species Range Ecoshapes (generalized)',
-  REVIEW = 'ReviewerApp2C - Review',
-  SPECIES = 'ReviewerApp2C - ReviewRangeMapSpecies',
+  REVIEW = 'ReviewerApp3Editable - Review',
+  SPECIES = 'ReviewerApp3ReadOnly - ReviewRangeMapSpecies',
   ECOSHAPE = 'Ecoshapes (generalized)',
-  ECOSHAPE_REVIEW = 'ReviewerApp2C - EcoshapeReview'
+  ECOSHAPE_REVIEW = 'ReviewerApp3Editable - EcoshapeReview'
 }
 
 export interface EcoshapeReview {
